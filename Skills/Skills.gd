b95a78f4dea3860ba5b98cf988402e792b08d87e
Sith <sithplgaming@gmail.com>
2025-08-10 17:08:50 -0500

    Add new skill animations and audio, refactor skill assets
    Introduces new skill animations and sound effects, including poison and melee skills, and updates animation resources to use new skill-specific assets instead of spell assets. Adds new textures and import files for Lancer and poison skills, updates animation frame data in Battle_Player.tres and Battle_Skills.tres, and adds corresponding audio files for new skills and effects. Also includes scene and script updates to support new skills and effects in battle.

--- Skills/Skills.gd
+++ Skills/Skills.gd
@@ -4,6 +4,9 @@ var ALL_SKILLS = {}
 
 func _ready():
 	# Load all skills manually or via directory scan
-	ALL_SKILLS["Heal"] = preload("res://Skills/Heal_Skill.tres")
-	ALL_SKILLS["Fireblast"] = preload("res://Skills/Fireblast_Skill.tres")
-	ALL_SKILLS["Volley"] = preload("res://Skills/Volley_Skill.tres")
+	ALL_SKILLS["Heal"] = preload("res://Skills/Heal_Skill.tres") # Priest
+	ALL_SKILLS["Fireblast"] = preload("res://Skills/Fireblast_Skill.tres") # Mage
+	ALL_SKILLS["Volley"] = preload("res://Skills/Volley_Skill.tres") # Ranger
+	ALL_SKILLS["Smite"] = preload("res://Skills/Smite_Skill.tres") # Paladin
+	ALL_SKILLS["Charge"] = preload("res://Skills/Charge_Skill.tres") # Lancer
+	ALL_SKILLS["P.Arrow"] = preload("res://Skills/PoisonArrow_Skill.tres") # Archer
