1bd24e203130faaf75f52c926cab42a8db870213
Sith <sithplgaming@gmail.com>
2025-08-10 23:12:09 -0500

    Add new skill SFX, update skill logic, and add Goon Cave
    Introduced new audio assets for skills and battle, updated skill resource files to use new SFX fields, and improved skill resolution logic in Battle.gd to support impact SFX for multi-target skills. Added the Dungeon_GoonCave scene and transitions, updated EnemyButton to play death SFX, and made minor map and data adjustments for new dungeon integration.

--- Battle/EnemyButton.gd
+++ Battle/EnemyButton.gd
@@ -1,6 +1,8 @@
 class_name EnemyButton extends BattleActorButton
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var sfx_player: AudioStreamPlayer = $AudioStreamPlayer
+@onready var sfx_enemy_death = load("res://Assets/Audio/Battle/sfx_enemy_death.wav")
 
 # Called when EnemyButton is added to the scene
 func _ready():
@@ -12,8 +14,10 @@ func _on_data_is_defeated():
 	#DEBUG print("EnemyButton.gd/_on_data_is_defeated() called")
 	if anim_player:
 		await get_tree().create_timer(1.0).timeout
+		sfx_player.stream = sfx_enemy_death
+		sfx_player.play()
 		anim_player.play("enemy_death")
-		await anim_player.animation_finished
+		await anim_player.animation_finished and sfx_player.finished
 	else:
 		print("AnimationPlayer is nil!")  # Wait briefly for animation, etc.
 	await get_tree().create_timer(0.5).timeout
